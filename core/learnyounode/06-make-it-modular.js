14786ca315e8a0a52f34f2d36998deae927dd175
Kei Kreutler <kei@ourmachine.net>
2014-09-15 21:25:29 +0200

    7th Heaven

--- core/learnyounode/06-make-it-modular.js
+++ core/learnyounode/06-make-it-modular.js
@@ -1,3 +1,13 @@
 var mymodule = require('./mymodule.js');
 
-function mymodule(process.argv[2], process.argv[3])
+var dir_name = process.argv[2];
+var file_ext = process.argv[3];
+
+mymodule(dir_name, file_ext, function(err, list) {
+  if (err) {
+   throw err;
+  }
+  else list.forEach(function (file) {
+   console.log(file);
+  })
+});
--- core/learnyounode/mymodule.js
+++ core/learnyounode/mymodule.js
@@ -1,13 +1,13 @@
 var fs = require('fs');
+var path = require('path');
 
 module.exports = function(dir_name, filename_ext, callback) {
 		var filename_array = new Array();
-		var filename_fitler_array = new Array();
+		var filename_filter_array = new Array();
 
 		fs.readdir(dir_name, function(error, list) {
 			if(error != null) {
-				console.log(error);
-				return;
+				return callback(error);
 			}
 			else {
 				for(var i=0; i < list.length; i++) {			
@@ -15,7 +15,7 @@ module.exports = function(dir_name, filename_ext, callback) {
 						filename_filter_array[i] = list[i];
 					}
 				}
-				return callback(null, filename_filter_array);
 			}
-		}
-	});
+			return callback(null, filename_filter_array);
+		})
+	};
